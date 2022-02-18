e2880258136dacd9872d24808d212febde6c89c6
mark-torres10 <markptorres1@gmail.com>
2022-02-17 19:20:26 -0500

    update basic layout of app skeleton

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
+import Router from "next/router";
+import NProgress from "nprogress";
+import { ChakraProvider } from '@chakra-ui/react';
+
+import Layout from '../src/Layout';
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  /* progress bars */
+  NProgress.configure({ showSpinner: false });
+  Router.events.on('routeChangeStart', () => {
+    NProgress.start();
+  });
+  Router.events.on('routeChangeComplete', () => {
+    NProgress.done();
+  })
+
+  /* display actual app */
+  return (
+    <ChakraProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
